1e6b14fc68262e2c37c20a551ad28650e2e0d9b7
Sisekocom <nonyongos1@gmail.com>
2023-11-30 12:21:24 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,22 @@
+
+function numbers() {
+    return new Promise((resolve, reject) => {
+    let number1 = prompt("Enter the first number:");
+    let number2 = prompt("Enter the second number:");
+         if (isNaN(number1) || isNaN(number2)) {
+            reject(`Both, or either ${isNaN(number1) ? number1 : number2} ${isNaN(number1) ? 'is' : 'are'} not number(s).`);
+          } else {
+            let sum = Number(number1) + Number(number2);
+            resolve(`${number1} + ${number2} = ${sum}`);
+          }
+        });
+      }
+numbers()
+.then(result => {
+  console.log(result);
+})
+.catch(error => {
+  console.error(error); 
+});
+
+  
